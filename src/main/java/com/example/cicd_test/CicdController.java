48486988fb82ddd04ec62f5c20918243d5b9ef43
pwoogi <chrishw1029@gmail.com>
2022-09-19 01:49:45 +0900

    도메인 에러, 수정후 커밋

--- src/main/java/com/example/cicd_test/CicdController.java
+++ src/main/java/com/example/cicd_test/CicdController.java
@@ -1,14 +1,16 @@
 package com.example.cicd_test;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CicdController {
 
+
     @GetMapping("/health")
-    public String healthChck(){
-        return "health";
+    public ResponseEntity<String> healthcheck(){
+        return ResponseEntity.ok("Please");
     }
 
 }
