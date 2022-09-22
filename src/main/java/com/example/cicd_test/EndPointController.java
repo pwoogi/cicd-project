4035a18da02337e87d70e538d7e074b568bfc7c9
pwoogi <chrishw1029@gmail.com>
2022-09-22 17:02:38 +0900

    도메인 에러, 수정후 커밋

---
+++ src/main/java/com/example/cicd_test/EndPointController.java
@@ -0,0 +1,21 @@
+package com.example.cicd_test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EndPointController {
+
+
+    @GetMapping("/end-point1")
+    public String endPoint1() {
+        return "Metrics for endPoint1";
+    }
+
+    @GetMapping("/end-point2")
+    public String endpoint2() {
+        return "Metrics for endPoint2";
+    }
+
+}
+
